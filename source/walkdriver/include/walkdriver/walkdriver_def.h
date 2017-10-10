00b0f8f720e2852f45f235cdff1e918bdcb9d2df
Evan <heyuanhui@outlook.com>
2017-10-10 18:10:04 +0800

    Update walkdriver_def.h

--- source/walkdriver/include/walkdriver/walkdriver_def.h
+++ source/walkdriver/include/walkdriver/walkdriver_def.h
@@ -7,9 +7,8 @@ namespace WalkDriver
     enum WalkType
     {
         WT_Unknown = 0,
-        WT_Straight,
-        WT_Normal,
-        WT_Fast,
+        WT_Run,
+        WT_Stop,
     };
 
     enum SpeedLevel
@@ -19,39 +18,5 @@ namespace WalkDriver
         SL_Normal,
         SL_Fast,
     };
-
-
-/**
-*  @brief
-*    Implement walk commands to driving wheels
-*/
-class WALKDRIVER_API Fibonacci
-{
-public:
-    /**
-    *  @brief
-    *    Constructor
-    */
-    Fibonacci();
-
-    /**
-    *  @brief
-    *    Destructor
-    */
-    virtual ~Fibonacci();
-
-    /**
-    *  @brief
-    *    Calculate fibonacci number
-    *
-    *  @param[in] i
-    *    Index
-    *
-    *  @return
-    *    Value of the i'th fibonacci number
-    */
-    unsigned int operator()(unsigned int i);
-};
-
-
+    
 } // namespace fiblib
