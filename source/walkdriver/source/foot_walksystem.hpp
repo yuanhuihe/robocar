087ad71c7b3aa54ebcb69f50ee3a9348d2efc0de
Evan <hyh618@gmail.com>
2017-10-20 15:58:17 +0800

    compiling errors

---
+++ source/walkdriver/source/foot_walksystem.hpp
@@ -0,0 +1,37 @@
+
+#pragma once
+
+#include <walkdriver/walkdriver.h>
+#include <walkdriver/walkdriver.h>
+
+namespace WalkDriver
+{
+    class FootWalkSystem : public WalkSystem
+    {
+    public:
+        FootWalkSystem()
+        {
+
+        }
+        virtual ~FootWalkSystem()
+        {
+
+        }
+
+        ExecutiveDevice* enumFirstExecutiveBody()
+        {
+
+            return nullptr;
+        }
+
+        ExecutiveDevice* enumNextExecutiveBody()
+        {
+
+            return nullptr;
+        }
+
+    private:
+
+    };
+
+} // namespace WalkDriver
