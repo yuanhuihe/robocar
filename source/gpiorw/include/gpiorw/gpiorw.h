e9988cf6704445bd2b06f86343e180bde6c5ca2b
yuanhui <hyh618@gmail.com>
2017-10-24 14:03:30 +0800

    io desc

--- source/gpiorw/include/gpiorw/gpiorw.h
+++ source/gpiorw/include/gpiorw/gpiorw.h
@@ -42,6 +42,9 @@ namespace GPIORW
     *
     *  @return
     *    -1 means error occured. Otherwise the retured value is readed from gpio.
+    *
+    * @remark
+    *   all input pin number is based on the Raspberry Pi 3b(40 pins) pins, start from 1, not 0.
     */
     GPIORW_API int GPIORead(int pin);
 
@@ -54,6 +57,9 @@ namespace GPIORW
     *
     *  @return
     *    -1 means error occured, 0 means successful.
+    *
+    * @remark
+    *   all input pin number is based on the Raspberry Pi 3b(40 pins) pins, start from 1, not 0.
     */
     GPIORW_API int GPIOWrite(int pin, int value);
 
